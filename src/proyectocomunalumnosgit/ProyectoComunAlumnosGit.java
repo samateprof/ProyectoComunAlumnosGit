523b434ed8b702ebb3a5a037cf8e9b058f728916
martaAguera <50086855+martaAguera@users.noreply.github.com>
2019-05-16 13:55:33 +0200

    Método de Marta

--- src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
+++ src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
@@ -17,9 +17,9 @@ public class ProyectoComunAlumnosGit {
     public static void main(String[] args) {
         System.out.println("Probando Github con múltiples colaboradores");
       
-        System.out.println("Método de Marta");
+       System.out.println("Método de Marta");
     }
 
 }
  
- 
+ }
